c1a0d7a097868700b18d09ac2071f0c7be203fe1
Jan Konczal <JanKonczal33@wp.pl>
2026-08-12 22:40:00 +0200

    dodanie pliku order.py z definicją tabeli Order

---
+++ app/models/order.py
@@ -0,0 +1,28 @@
+import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.orm import Mapped
+from app.database import Base
+from decimal import Decimal
+from sqlalchemy import Numeric
+from app.enums import OrderStatus
+
+class Order(Base):
+    __tablename__ = "orders"
+    order_id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"),nullable=False)
+    total_price: Mapped[Decimal] = mapped_column(Numeric(10,2),default=Decimal("0.00"),nullable=False)
+    status: Mapped[OrderStatus] = mapped_column(nullable=False,default=OrderStatus.PENDING)
+    order_date: Mapped[datetime.datetime] = mapped_column(nullable=False, default= lambda: datetime.datetime.now(datetime.UTC))
+    modification_date: Mapped[datetime.datetime] = mapped_column(nullable=False, onupdate= lambda: datetime.datetime.now(datetime.UTC))
+    street: Mapped[str] = mapped_column(nullable=False)
+    building_number: Mapped[str] = mapped_column(nullable=False)
+    apartment_number: Mapped[str] = mapped_column(nullable=True)
+    postal_code: Mapped[str] = mapped_column(nullable=False)
+    city: Mapped[str] = mapped_column(nullable=False)
+    country: Mapped[str] = mapped_column(nullable=False)
+    user: Mapped["User"] = relationship(back_populates="orders")
+    order_items: Mapped[list["OrderItem"]] = relationship(back_populates="order")
+    changes_history: Mapped[list["OrderStatusHistory"]] = relationship(back_populates="order")
+
+
