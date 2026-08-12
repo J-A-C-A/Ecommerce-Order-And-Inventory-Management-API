b3abc5da02f0c6d17f9dd44ee9e03d3b4e63a2ff
Jan Konczal <JanKonczal33@wp.pl>
2026-08-12 22:38:19 +0200

    dodanie pliku cart_item.py z definicją tabeli CartItem

---
+++ app/models/cart_item.py
@@ -0,0 +1,18 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.orm import Mapped
+from app.database import Base
+from decimal import Decimal
+from sqlalchemy import Numeric
+from sqlalchemy import UniqueConstraint
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    __table_args__ = (UniqueConstraint("product_id", "cart_id"),)
+    cartItem_id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.product_id"),nullable=False)
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.cart_id"),nullable=False)
+    product_quantity: Mapped[int] = mapped_column(nullable=False)
+    price: Mapped[Decimal] = mapped_column(Numeric(10,2),default=Decimal("0.00"),nullable=False)
+    product: Mapped["Product"] = relationship( back_populates="cart_items")
+    cart: Mapped["Cart"] = relationship(back_populates="cart_items")
