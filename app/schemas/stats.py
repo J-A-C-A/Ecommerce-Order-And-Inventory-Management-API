d5cbbffb1c1453c198d86a9981a440720441dfb4
Jan Konczal <JanKonczal33@wp.pl>
2026-08-13 23:14:37 +0200

    dodano plik stats.py ze schematami pydantic dla statystyk liczonych na podstawie danych z bazy

---
+++ app/schemas/stats.py
@@ -0,0 +1,19 @@
+import datetime
+from decimal import Decimal
+from pydantic import BaseModel
+from app.enums import OrderStatus
+from app.schemas.product import ProductResponse
+
+
+class TopProductResponse(BaseModel):
+    product: ProductResponse
+    total_sold: int
+
+class RevenueResponse(BaseModel):
+    from_start: datetime.datetime
+    to_end: datetime.datetime
+    total_revenue: Decimal
+
+class OrderCountByStatus(BaseModel):
+    status: OrderStatus
+    count: int
