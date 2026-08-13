481b991eb7ac5ea7ca502ca3e79256b103c84dec
Jan Konczal <JanKonczal33@wp.pl>
2026-08-13 23:13:31 +0200

    dodano plik product.py ze schematami pydantic dla Product

---
+++ app/schemas/product.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from decimal import Decimal
+from app.schemas.category import CategoryResponse
+from pydantic import ConfigDict
+from typing import Optional
+
+class ProductCreate(BaseModel):
+    product_name: str
+    product_description: str
+    is_active: bool
+    price: Decimal
+    category_id: int
+
+class ProductResponse(BaseModel):
+    product_id: int
+    product_name: str
+    product_description: str
+    is_active: bool
+    price: Decimal
+    category: CategoryResponse
+    model_config = ConfigDict(from_attributes=True)
+
+class ProductAdminUpdate(BaseModel):
+    product_id: Optional[int] = None
+    product_name: Optional[str] = None
+    product_description: Optional[str] = None
+    is_active: Optional[bool] = None
+    price: Optional[Decimal] = None
+    category_id: Optional[int] = None
+
+class ProductListResponse(BaseModel):
+    products: list[ProductResponse]
+    number_of_pages: int
+    page_size: int
+    total: int
+
