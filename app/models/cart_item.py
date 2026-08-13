5a1a0808020ba5e6142744415465d802d76a8141
Jan Konczal <JanKonczal33@wp.pl>
2026-08-13 23:06:46 +0200

    usunięto kolumnę price z tabeli CartItem i wykonano migrację

--- app/models/cart_item.py
+++ app/models/cart_item.py
@@ -2,8 +2,6 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy.orm import Mapped
 from app.database import Base
-from decimal import Decimal
-from sqlalchemy import Numeric
 from sqlalchemy import UniqueConstraint
 
 class CartItem(Base):
@@ -13,6 +11,5 @@ class CartItem(Base):
     product_id: Mapped[int] = mapped_column(ForeignKey("products.product_id"),nullable=False)
     cart_id: Mapped[int] = mapped_column(ForeignKey("carts.cart_id"),nullable=False)
     product_quantity: Mapped[int] = mapped_column(nullable=False)
-    price: Mapped[Decimal] = mapped_column(Numeric(10,2),default=Decimal("0.00"),nullable=False)
     product: Mapped["Product"] = relationship( back_populates="cart_items")
     cart: Mapped["Cart"] = relationship(back_populates="cart_items")
