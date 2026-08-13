c75cc76e81760b99c73b699ddf8282da8c042af4
Jan Konczal <JanKonczal33@wp.pl>
2026-08-13 23:08:17 +0200

    dodano plik cart.py ze schematami pydantic dla Cart i CartItem

---
+++ app/schemas/cart.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel
+from decimal import Decimal
+from app.schemas.product import ProductResponse
+from pydantic import ConfigDict
+from typing import Optional
+
+class CartItemCreate(BaseModel):
+    product_id: int
+    product_quantity: int
+
+class CartItemCustomerUpdate(BaseModel):
+    product_id: Optional[int] = None
+    product_quantity: Optional[int] = None
+
+class CartItemResponse(BaseModel):
+    product: ProductResponse
+    product_quantity: int
+    total_item_price: Decimal
+    model_config = ConfigDict(from_attributes=True)
+
+class CartResponse(BaseModel):
+    cart_items: list[CartItemResponse]
+    total_cart_price: Decimal
+    model_config = ConfigDict(from_attributes=True)
