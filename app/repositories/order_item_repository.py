b6aabba56374000a3e69e44f00d6f165fd4f27b0
Jan Konczal <JanKonczal33@wp.pl>
2026-08-18 22:56:59 +0200

    dodano plik order_item_repository.py gdzie zdefiniowano warstwę kontaktu z tabelą OrderItem

---
+++ app/repositories/order_item_repository.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models import OrderItem
+
+
+class OrderItemRepository():
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create_order_item(self, new_item: OrderItem) -> OrderItem:
+        self.db.add(new_item)
+        await self.db.commit()
+        await self.db.refresh(new_item)
+        return new_item
+
+    async def create_all(self, new_items: list[OrderItem]) -> list[OrderItem]:
+        self.db.add_all(new_items)
+        await self.db.commit()
+        for item in new_items:
+            await self.db.refresh(item)
+        return new_items
