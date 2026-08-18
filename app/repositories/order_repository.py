b9f502c01519c74b98c7e828933ccea24e619693
Jan Konczal <JanKonczal33@wp.pl>
2026-08-18 22:57:47 +0200

    dodano plik order_repository.py gdzie zdefiniowaną warstwę kontaktu z tabelą Order

---
+++ app/repositories/order_repository.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+from app.models import Order, OrderItem
+
+class OrderRepository():
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_by_id(self, order_id: int) -> Order | None:
+        query = select(Order).options(selectinload(Order.order_items).selectinload(OrderItem.product)).where(Order.order_id == order_id)
+        result = await self.db.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_all_by_user(self, user_id: int) -> list[Order]:
+        query = select(Order).options(selectinload(Order.order_items).selectinload(OrderItem.product)).where(Order.user_id == user_id)
+        result = await self.db.execute(query)
+        return result.scalars().all()
+
+    async def create_order(self, new_order: Order) -> Order:
+        self.db.add(new_order)
+        await self.db.commit()
+        await self.db.refresh(new_order)
+        return new_order
+
+    async def update_order(self, order: Order) -> Order:
+        await self.db.commit()
+        await self.db.refresh(order)
+        return order
