2846d98f4d3597390e4fd1ca6e277c75ebab1b22
Jan Konczal <JanKonczal33@wp.pl>
2026-08-18 23:00:30 +0200

    dodano plik user_repository.py gdzie zdefiniowano warstwę kontaktu z tabelą User

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
+from sqlalchemy import select
+
+class UserRepository():
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_by_email(self, email: str) -> User | None:
+        query = select(User).where(User.email == email)
+        result = await self.db.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_by_id(self, user_id: int) -> User | None:
+        query = select(User).where(User.user_id == user_id)
+        result = await self.db.execute(query)
+        return result.scalar_one_or_none()
+
+    async def create_user(self, new_user: User) -> User:
+        self.db.add(new_user)
+        await self.db.commit()
+        await self.db.refresh(new_user)
+        return new_user
