fb42e255cc73f573bad2575ea310855d21cbaf70
Jan Konczal <JanKonczal33@wp.pl>
2026-08-18 22:59:00 +0200

    dodano plik order_status_history_repository.py gdzie zdefiniowano warstwę kontaktu z tabelą OrderStatysHistory

---
+++ app/repositories/order_status_history_repository.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.models import OrderStatusHistory
+
+class OrderStatusHistoryRepository():
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_all_for_order(self, order_id: int) -> list[OrderStatusHistory]:
+        query = select(OrderStatusHistory).where(OrderStatusHistory.order_id == order_id)
+        result = await self.db.execute(query)
+        return result.scalars().all()
+
+    async def create_order_status_history(self, history_entry: OrderStatusHistory) -> OrderStatusHistory:
+        self.db.add(history_entry)
+        await self.db.commit()
+        await self.db.refresh(history_entry)
+        return history_entry
