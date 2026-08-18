8b22457c07b2cb1a1edc53ea5809043ca9c5f2bf
Jan Konczal <JanKonczal33@wp.pl>
2026-08-18 22:44:17 +0200

    dodanie pliku cart_repository.py gdzie zdefiniowaną warstwę kontaktu z tabelą Cart

---
+++ app/repositories/cart_repository.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+from app.models import Cart, CartItem
+
+
+class CartRepository():
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_by_user_id(self, user_id: int) -> Cart | None:
+        query = select(Cart).options(selectinload(Cart.cart_items).selectinload(CartItem.product)).where(Cart.user_id == user_id)
+        result = await self.db.execute(query)
+        return result.scalar_one_or_none()
+
+    async def create_cart(self, new_cart: Cart) -> Cart:
+        self.db.add(new_cart)
+        await self.db.commit()
+        await self.db.refresh(new_cart)
+        return new_cart
