d7a84db97acde1ac97c350f5165b5516a5f20202
Jan Konczal <JanKonczal33@wp.pl>
2026-08-20 16:55:23 +0200

    dodanie pliku auth.py zawierającego definicje endpointów do rejestracji i logowania użytkownika

---
+++ app/api/auth.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from app.api.deps import get_auth_service
+from app.schemas.auth import Token
+from app.schemas.user import UserResponse, UserCreate
+from app.services.auth_service import AuthService
+
+
+router = APIRouter()
+
+@router.post("/register")
+async def register(user_data: UserCreate, auth_service: AuthService = Depends(get_auth_service)) -> UserResponse:
+    new_user = await auth_service.register_user(user_data)
+    return new_user
+
+@router.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), auth_service: AuthService = Depends(get_auth_service)) -> Token:
+    user_token = await auth_service.login_user(form_data.username, form_data.password)
+    return user_token
+
