5951b925d4ae0cc127510abbd103329f17c10b14
Jan Konczal <JanKonczal33@wp.pl>
2026-08-21 17:46:49 +0200

    dodanie zależności get_product_repository, get_inventory_repository, get_product_service

--- app/api/deps.py
+++ app/api/deps.py
@@ -4,9 +4,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.enums import RoleType
 from app.models.user import User
 from app.repositories.category_repository import CategoryRepository
+from app.repositories.inventory_repository import InventoryRepository
+from app.repositories.product_repository import ProductRepository
 from app.repositories.user_repository import UserRepository
 from app.services.auth_service import AuthService
 from app.services.category_service import CategoryService
+from app.services.product_service import ProductService
 from app.utils.security import decode_token
 from app.database import get_db
 
@@ -18,6 +21,11 @@ async def get_user_repository(db: AsyncSession = Depends(get_db)) -> UserReposit
 async def get_category_repository(db: AsyncSession = Depends(get_db)) -> CategoryRepository:
     return CategoryRepository(db=db)
 
+async def get_product_repository(db: AsyncSession = Depends(get_db)) -> ProductRepository:
+    return ProductRepository(db=db)
+
+async def get_inventory_repository(db: AsyncSession = Depends(get_db)) -> InventoryRepository:
+    return InventoryRepository(db=db)
 
 async def get_current_user(
         token: str = Depends(oauth2_scheme),
@@ -52,3 +60,6 @@ async def get_auth_service(repo: UserRepository = Depends(get_user_repository)) 
 
 async def get_category_service(repo: CategoryRepository = Depends(get_category_repository)) -> CategoryService:
     return CategoryService(category_repository=repo)
+
+async def get_product_service(db: AsyncSession = Depends(get_db), product_repository: ProductRepository = Depends(get_product_repository), inventory_repository: InventoryRepository = Depends(get_inventory_repository)) -> ProductService:
+    return ProductService(db= db, product_repository=product_repository, inventory_repository=inventory_repository)
