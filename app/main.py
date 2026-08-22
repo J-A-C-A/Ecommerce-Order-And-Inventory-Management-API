96ea23d49065c95d0ece2e09661b7d044f5dcab4
Jan Konczal <JanKonczal33@wp.pl>
2026-08-22 13:04:42 +0200

    podłączenie endpointów inventory

--- app/main.py
+++ app/main.py
@@ -3,8 +3,12 @@ from app.api.health_check import router as health_router
 from app.api.auth import router as auth_router
 from app.api.categories import router as categories_router
 from app.api.products import router as products_router
+from app.api.inventory import router as inventory_router
 app = FastAPI()
 app.include_router(health_router)
 app.include_router(auth_router)
 app.include_router(categories_router)
 app.include_router(products_router)
+app.include_router(inventory_router)
+
+#uvicorn app.main:app --reload
