d4752375648829475207a8f26f899a394b71c8f2
Jan Konczal <JanKonczal33@wp.pl>
2026-08-25 13:39:09 +0200

    podłączenie endpointów cart

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from app.api.auth import router as auth_router
 from app.api.categories import router as categories_router
 from app.api.products import router as products_router
 from app.api.inventory import router as inventory_router
+from app.api.cart import router as cart_router
+
 app = FastAPI()
 app.include_router(health_router)
 app.include_router(auth_router)
 app.include_router(categories_router)
 app.include_router(products_router)
 app.include_router(inventory_router)
-
+app.include_router(cart_router)
 #uvicorn app.main:app --reload
