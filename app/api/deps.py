7f47586023d4e7ce289cd5b8ea59f4d0a065c636
Jan Konczal <JanKonczal33@wp.pl>
2026-08-26 20:01:09 +0200

    dodanie zależności get_order_repository, get_order_item_repository, get_order_status_history_repository, get_order_service

--- app/api/deps.py
+++ app/api/deps.py
@@ -9,11 +9,15 @@ from app.repositories.product_repository import ProductRepository
 from app.repositories.user_repository import UserRepository
 from app.repositories.cart_repository import CartRepository
 from app.repositories.cart_item_repository import CartItemRepository
+from app.repositories.order_repository import OrderRepository
+from app.repositories.order_status_history_repository import OrderStatusHistoryRepository
+from app.repositories.order_item_repository import OrderItemRepository
 from app.services.auth_service import AuthService
 from app.services.category_service import CategoryService
 from app.services.inventory_service import InventoryService
 from app.services.product_service import ProductService
 from app.services.cart_service import CartService
+from app.services.order_service import OrderService
 from app.utils.security import decode_token
 from app.database import get_db
 
@@ -36,6 +40,15 @@ async def get_cart_repository(db: AsyncSession = Depends(get_db)) -> CartReposit
 
 async def get_cart_item_repository(db: AsyncSession = Depends(get_db)) -> CartItemRepository:
     return CartItemRepository(db=db)
+
+async def get_order_repository(db: AsyncSession = Depends(get_db)) -> OrderRepository:
+    return OrderRepository(db=db)
+
+async def get_order_item_repository(db: AsyncSession = Depends(get_db)) -> OrderItemRepository:
+    return OrderItemRepository(db=db)
+
+async def get_order_status_history_repository(db: AsyncSession = Depends(get_db)) -> OrderStatusHistoryRepository:
+    return OrderStatusHistoryRepository(db=db)
 
 async def get_current_user(
         token: str = Depends(oauth2_scheme),
@@ -79,3 +92,25 @@ async def get_inventory_service(db: AsyncSession = Depends(get_db), inventory_re
 
 async def get_cart_service(db: AsyncSession = Depends(get_db), cart_repository: CartRepository = Depends(get_cart_repository), cart_item_repository: CartItemRepository = Depends(get_cart_item_repository), product_repository: ProductRepository = Depends(get_product_repository)) -> CartService:
     return CartService(db=db,cart_repository=cart_repository, cart_item_repository=cart_item_repository, product_repository=product_repository)
+
+async def get_order_service(db: AsyncSession = Depends(get_db),
+                            order_repository: OrderRepository = Depends(get_order_repository),
+                            order_item_repository: OrderItemRepository = Depends(get_order_item_repository),
+                            order_status_history: OrderStatusHistoryRepository = Depends(get_order_status_history_repository),
+                            cart_repository: CartRepository = Depends(get_cart_repository),
+                            cart_item_repository: CartItemRepository = Depends(get_cart_item_repository),
+                            inventory_repository: InventoryRepository = Depends(get_inventory_repository),
+                            inventory_service: InventoryService = Depends(get_inventory_service),
+                            cart_service: CartService = Depends(get_cart_service)) -> OrderService:
+    return OrderService(db=db,
+                        order_repository= order_repository,
+                        order_item_repository= order_item_repository,
+                        order_status_history_repository= order_status_history,
+                        cart_repository= cart_repository,
+                        cart_item_repository= cart_item_repository,
+                        inventory_repository= inventory_repository,
+                        inventory_service= inventory_service,
+                        cart_service= cart_service)
+
+
+
