1aa02e6f9952ef82c25069559ac624af9158e108
Jan Konczal <JanKonczal33@wp.pl>
2026-08-27 17:37:42 +0200

    podłączenie endpointów stats

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.api.products import router as products_router
 from app.api.inventory import router as inventory_router
 from app.api.cart import router as cart_router
 from app.api.order import router as order_router
+from app.api.endpoints_stats import router as stats_router
 
 app = FastAPI()
 app.include_router(health_router)
@@ -15,4 +16,5 @@ app.include_router(products_router)
 app.include_router(inventory_router)
 app.include_router(cart_router)
 app.include_router(order_router)
+app.include_router(stats_router)
 #uvicorn app.main:app --reload
