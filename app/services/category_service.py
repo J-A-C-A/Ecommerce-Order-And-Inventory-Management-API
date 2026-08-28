c89c56bb4bea30497bc98b057f18b97b33d1b5e1
Jan Konczal <JanKonczal33@wp.pl>
2026-08-28 16:54:30 +0200

    funkcja get_all_categories najpierw sprawdza czy w Redis są zapisane wszystkie kategorie, jeśli tak to zwraca je z cache, jeśli nie to pobiera z bazy danych i zapisuje w cache po czym zwraca wynik. Funkcje create_category, update_category oraz delete_category czyszczą pamieć cache po dokonaniu zmiany w bazie

--- app/services/category_service.py
+++ app/services/category_service.py
@@ -2,7 +2,8 @@ from fastapi import HTTPException, status
 from app.models import Category
 from app.repositories.category_repository import CategoryRepository
 from app.schemas.category_schema import CategoryCreate, CategoryResponse, CategoryAdminUpdate
-
+import json
+from app.utils.cache import redis_client
 class CategoryService():
     def __init__(self, category_repository: CategoryRepository):
         self.category_repo = category_repository
@@ -13,6 +14,7 @@ class CategoryService():
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Category already exists")
         new_category = Category(category_name=category_data.category_name)
         registered_category = await self.category_repo.create_category(new_category)
+        await redis_client.delete("categories:all")
         return CategoryResponse.model_validate(registered_category)
 
     async def get_category(self, category_id: int) -> CategoryResponse:
@@ -29,6 +31,7 @@ class CategoryService():
         for field_name, value in update_fields.items():
             setattr(category, field_name, value)
         updated_category = await self.category_repo.update_category(category)
+        await redis_client.delete("categories:all")
         return CategoryResponse.model_validate(updated_category)
 
     async def delete_category(self, category_id: int) -> None:
@@ -36,8 +39,19 @@ class CategoryService():
         if category is None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
         await self.category_repo.delete_category(category)
+        await redis_client.delete("categories:all")
 
     async def get_all_categories(self) -> list[CategoryResponse]:
+        cache_data = await redis_client.get("categories:all")
+        if cache_data is not None:
+            list_of_dicts = json.loads(cache_data)
+            list_of_categories = [CategoryResponse(**dict_) for dict_ in list_of_dicts]
+            return list_of_categories
+
         categories = await self.category_repo.get_all()
-        return [CategoryResponse.model_validate(category) for category in categories]
+        result = [CategoryResponse.model_validate(category) for category in categories]
+        list_of_dicts = [category.model_dump() for category in result]
+        json_string = json.dumps(list_of_dicts,default=str)
+        await redis_client.set("categories:all", json_string)
+        return result
     
