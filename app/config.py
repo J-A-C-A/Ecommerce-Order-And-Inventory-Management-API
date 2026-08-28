422e6f3ad25a0de55f45f0191f292d01261c5dd9
Jan Konczal <JanKonczal33@wp.pl>
2026-08-28 16:45:37 +0200

    dodanie pól REDIS_HOST, REDIS_PORT i CELERY_BROKER_URL do klasy Settings

--- app/config.py
+++ app/config.py
@@ -13,7 +13,9 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    CELERY_BROKER_URL: str = "redis://localhost:6379/1"
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env"
     )
