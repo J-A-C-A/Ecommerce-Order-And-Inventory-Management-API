bd8ee5b9eafbf8439e258da1fed580f4170b0ea7
Jan Konczal <JanKonczal33@wp.pl>
2026-09-01 22:00:06 +0200

    zmiana sposobu tworzenia klienta Redis — zastąpienie globalnego klienta Redis funkcją get_redis_client() tworzącą nową instancję klienta na żądanie

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -1,4 +1,11 @@
 import redis.asyncio as redis
 from app.config import settings
 
-redis_client = redis.Redis(host=settings.REDIS_HOST,port=settings.REDIS_PORT,decode_responses=True)
+def get_redis_client():
+    return redis.Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        decode_responses=True
+    )
+
+#redis_client = redis.Redis(host=settings.REDIS_HOST,port=settings.REDIS_PORT,decode_responses=True)
